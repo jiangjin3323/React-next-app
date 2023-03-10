8bf89c70bf895cc7d8892352675b0b1cb8a5f816
姜衿 <70458277@qq.com>
2023-03-10 17:40:01 +0800

    官网初步完善。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,40 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Layout } from "antd";
+import Link from "next/link";
+const { Header, Content } = Layout;
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout>
+        <Header className="bg-white w-full flex items-center justify-center	">
+            <div className="flex items-center	w-[1200px] h-20	justify-between">
+                <div className="flex items-center justify-center">
+                   <img className="w-40 h-11" src="http://www.sl66.cn/img/logo.png?1677837484" alt="美优到家" />
+                </div>
+                <div className="flex items-center justify-between	">
+                  <Link href={'/'}>
+                    <div className="flex items-center justify-center w-40 h-11">首页</div>
+                  </Link>
+                  <Link href={'/'}>
+                    <div className="flex items-center justify-center w-40 h-11 text-[#17233d]">产品服务</div>
+                  </Link>
+                  <Link href={'/'}>
+                    <div className="flex items-center justify-center w-40 h-11 text-[#17233d]">技师展示</div>
+                  </Link>
+                  <Link href={'/'}>
+                    <div className="flex items-center justify-center w-40 h-11 text-[#17233d]">关于我们</div>
+                  </Link>
+                  <Link href={'/'}>
+                    <div className="flex items-center justify-center w-40 h-11 text-[#17233d]">合作加盟</div>
+                  </Link>
+                </div>
+            </div>
+        </Header>
+        <Content>
+          <Component {...pageProps} />
+        </Content>
+      </Layout>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,136 @@
-import type { NextPage } from 'next'
-import { Layout } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
+import type { NextPage } from "next";
+import { Carousel } from "antd";
+import { useState } from "react";
 const Index: NextPage = () => {
-  return (
-    <Layout>
-      <Header>header</Header>
-      <Content>main content</Content>
-      <Footer>footer</Footer>
-    </Layout>
-  )
-}
+  const [swiperList, setSwiperList]: [
+    swiperList: string[],
+    setSwiperList: any
+  ] = useState([
+    "https://img.youpin.mi-img.com/ferriswheel/4bbc32cf_1fe0_45aa_a48f_74b9282edf9f.jpeg?w=2560&h=1080",
+    "https://img.youpin.mi-img.com/ferriswheel/f9e992cd_8a4b_4c54_81b8_a1e11ce05c06.jpeg@base@tag=imgScale&F=webp&h=1080&q=90&w=2560",
+    "https://wds-service-1258344699.file.myqcloud.com/20/15204/png/16771155064393955ae135ef021ce.png?version=1677115510",
+  ]);
 
-export default Index
+  const [advantageList, setAdvantageList]: [
+    advantageList: { img: string; title: string; text: string }[],
+    setAdvantageList: any
+  ] = useState([
+    {
+      img: "https://wds-service-1258344699.file.myqcloud.com/20/15204/jpg/167763868973736141f114521ec4b.jpg?version=1677638692",
+      title: "品质服务",
+      text: "高品质服务项目，满足您的按摩需求，包含休闲按摩、疼痛理疗、女性保养等三大类",
+    },
+    {
+      img: "https://wds-service-1258344699.file.myqcloud.com/20/15204/jpeg/1677641259156b6f04f8dd40d7d90.jpeg?version=0",
+      title: "专业技师",
+      text: "资深专业理疗师，缓解您的疼痛不适，专业资质认证，正规安全更方便",
+    },
+    {
+      img: "https://wds-service-1258344699.file.myqcloud.com/20/15204/png/16776393239142afb9ab9dc38928c.png?version=0",
+      title: "一键下单",
+      text: "专业线上平台，一键预约，轻松上门，享受超5星按摩推拿服务，解放时间，简单方便",
+    },
+  ]);
+
+  const [featuredList, setFeaturedList]: [
+    featuredList: { img: string; title: string; text: string }[],
+    setFeaturedList: any
+  ] = useState([
+    {
+      img: "https://wds-service-1258344699.file.myqcloud.com/20/15204/jpg/1677825157440504f583cda746597.jpg?version=1677825159",
+      title: "休闲按摩",
+      text: "休闲养生 适用于亚健康人群",
+    },
+    {
+      img: "https://wds-service-1258344699.file.myqcloud.com/20/15204/png/16777195567354152cb9fd569ac28.png?version=1677719558",
+      title: "康养推拿",
+      text: "疼痛按摩 适用于局部疼痛活动受限人群",
+    },
+    {
+      img: "https://wds-service-1258344699.file.myqcloud.com/20/15204/png/1677722438835ea722525a91bc110.png?version=1677722440",
+      title: "女性保养",
+      text: "女性专属 纤体瘦身抗衰",
+    },
+  ]);
+  return (
+    <div>
+      <Carousel autoplay>
+        {swiperList.map((item, index) => {
+          return (
+            <div className="w-full h-[530px]" key={index}>
+              <img className="w-full h-[530px]" src={item} alt="" />
+            </div>
+          );
+        })}
+      </Carousel>
+      <div className="w-full flex items-center justify-center bg-white	">
+        <div className="w-[1200px]">
+          <div>
+            <div className="text-center	text-4xl text-[#4a4a4a] py-7">
+              我们的优势
+            </div>
+            <div className="text-center	text-xl">
+              24小时 · 专业正规 · 不收小费
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mt-10">
+            {advantageList.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="shadow-[0_2px_10px_0_rgb(25_93_194_/_7%)]
+bg-white border border-[#e9eaf3] rounded-[30px] w-auto inline-block transition-all duration-300 hover:scale-105 group !flex !flex-col justify-between p-[20px_36px]"
+                >
+                  <img className="rounded-full" src={item.img} alt="" />
+                  <div className="text-[#ff7f00] font-bold text-2xl	py-3 text-center">
+                    {item.title}
+                  </div>
+                  <div className="text-[#999999] text-lg text-center	">
+                    {item.text}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            <div className="text-center	text-4xl text-[#4a4a4a] py-7">
+              精选服务
+            </div>
+            <div className="text-center	text-xl">
+              休闲养生 · 疼痛按摩 · 女性专享，全人群，全需求
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mt-10">
+            {featuredList.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="shadow-[0_2px_10px_0_rgb(25_93_194_/_7%)]
+bg-white border border-[#e9eaf3] rounded-[30px] w-auto inline-block transition-all duration-300 hover:scale-105 group !flex !flex-col justify-between p-[20px_36px]"
+                >
+                  <img src={item.img} alt="" />
+                  <div className="text-[#ff7f00] font-bold text-2xl	py-3 text-center">
+                    {item.title}
+                  </div>
+                  <div className="text-[#999999] text-lg text-center	">
+                    {item.text}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            <div className="text-center	text-4xl text-[#4a4a4a] py-7">
+              严选技师
+            </div>
+            <div className="text-center	text-xl">
+              专业高品质技师，手法娴熟，给你超凡舒爽体验
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
